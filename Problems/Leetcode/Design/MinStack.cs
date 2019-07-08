8797c7c3184636305166ded5c379e51c1cf7df87
anas shaheen <anasshaheen97@outlook.com>
2019-07-08 15:54:25 +0300

    Finish design problems.(top-interview-questions-easy)

---
+++ Problems/Leetcode/Design/MinStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS.Problems.Leetcode.Design
+{
+    public class MinStack
+    {
+        private readonly Stack<int> _stack;
+        private readonly Stack<int> _minStack;
+
+        public MinStack()
+        {
+            _stack = new Stack<int>();
+            _minStack = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            _stack.Push(x);
+            if (!_minStack.Any())
+            {
+                _minStack.Push(x);
+            }
+            else
+            {
+                _minStack.Push(Math.Min(x, _minStack.Peek()));
+            }
+        }
+
+        public void Pop()
+        {
+            if (_stack.Any())
+            {
+                _minStack.Pop();
+                _stack.Pop();
+            }
+
+        }
+
+        public int Top()
+        {
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _minStack.Peek();
+        }
+    }
+}
---
+++ Problems/Leetcode/Design/ShuffleAnArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CS.Problems.Leetcode.Design
+{
+    public class ShuffleAnArray
+    {
+        private readonly int[] _numbers;
+        private readonly Random _random;
+
+        public ShuffleAnArray(int[] numbers)
+        {
+            _numbers = numbers;
+            _random = new Random();
+        }
+
+        public int[] Reset()
+        {
+            var copiedArr = new int[_numbers.Length];
+            _numbers.CopyTo(copiedArr, 0);
+
+            return copiedArr;
+        }
+
+        public int[] Shuffle()
+        {
+            var copiedArr = new int[_numbers.Length];
+            _numbers.CopyTo(copiedArr, 0);
+
+            for (var i = 0; i < copiedArr.Length; i++)
+            {
+                var randomIndex = _random.Next(i + 1);
+
+                var temp = copiedArr[randomIndex];
+                copiedArr[randomIndex] = copiedArr[i];
+                copiedArr[i] = temp;
+            }
+
+            return copiedArr;
+        }
+    }
+}
